ac165db54e08077be75cdbff52fb13738a0a26f4
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:40:17 +0200

    Create Lesson9.java

---
+++ src/Lesson9.java
@@ -0,0 +1,36 @@
+public class Lesson9 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 9: Strings");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("Strings store text.");
+
+        System.out.println();
+        System.out.println("Useful methods:");
+        System.out.println(".length()");
+        System.out.println(".toUpperCase()");
+        System.out.println(".toLowerCase()");
+        System.out.println(".substring()");
+        System.out.println(".contains()");
+
+        System.out.println();
+        System.out.println("Example:");
+        System.out.println("String name = \"Java\";");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Convert a name to uppercase.");
+
+        System.out.println();
+        System.out.println("Challenge:");
+        System.out.println("Count the number of characters");
+        System.out.println("in a sentence.");
+
+        System.out.println();
+        System.out.println("End of Lesson 9.");
+    }
+}

124e95d6ddf2b8501df66e8f13ca9b9b7c4811cd
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 21:22:10 +0200

    Create LessonMenu.java

---
+++ src/LessonMenu.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class LessonMenu {
+
+    public void showLessons() {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("==============================");
+        System.out.println("        JAVA LESSONS");
+        System.out.println("==============================");
+        System.out.println("1. What is Java?");
+        System.out.println("2. Variables");
+        System.out.println("3. Data Types");
+        System.out.println("4. Operators");
+        System.out.println("5. If Statements");
+        System.out.println("6. Loops");
+        System.out.println("7. Methods");
+        System.out.println("8. Arrays");
+        System.out.println("9. Classes & Objects");
+        System.out.println("10. Object-Oriented Programming");
+        System.out.println("0. Back");
+
+        System.out.print("\nChoose a lesson: ");
+        int choice = input.nextInt();
+
+        switch (choice) {
+            case 1:
+                Lesson lesson = new Lesson("Lesson 1: What is Java?");
+                lesson.startLesson();
+                break;
+
+            default:
+                System.out.println("This lesson will be added soon.");
+        }
+    }
+}

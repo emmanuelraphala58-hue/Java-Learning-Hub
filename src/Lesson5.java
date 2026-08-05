d4407ee1b94959834709ae5b8c0ceaef0c72a802
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:30:05 +0200

    Create Lesson5.java

---
+++ src/Lesson5.java
@@ -0,0 +1,69 @@
+public class Lesson5 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 5: If Statements");
+        System.out.println("=========================");
+
+        System.out.println();
+
+        System.out.println("If statements allow a program");
+        System.out.println("to make decisions based on conditions.");
+
+        System.out.println();
+
+        System.out.println("Basic Syntax:");
+
+        System.out.println("if (condition) {");
+        System.out.println("    // code executes");
+        System.out.println("}");
+
+        System.out.println();
+
+        System.out.println("Example:");
+
+        System.out.println("int age = 18;");
+        System.out.println("if (age >= 18) {");
+        System.out.println("    System.out.println(\"You are an adult.\");");
+        System.out.println("}");
+
+        System.out.println();
+
+        System.out.println("Java also supports:");
+        System.out.println("- if");
+        System.out.println("- if...else");
+        System.out.println("- if...else if...else");
+
+        System.out.println();
+
+        System.out.println("Key Notes:");
+        System.out.println("- Conditions return true or false.");
+        System.out.println("- Comparison operators include:");
+        System.out.println("  ==  Equal to");
+        System.out.println("  !=  Not equal to");
+        System.out.println("  >   Greater than");
+        System.out.println("  <   Less than");
+        System.out.println("  >=  Greater than or equal to");
+        System.out.println("  <=  Less than or equal to");
+
+        System.out.println();
+
+        System.out.println("Practice Exercise:");
+        System.out.println("Write a program that checks");
+        System.out.println("if a student's mark is");
+        System.out.println("50 or above and prints PASS,");
+        System.out.println("otherwise print FAIL.");
+
+        System.out.println();
+
+        System.out.println("Challenge:");
+        System.out.println("Write a program that");
+        System.out.println("determines if a person");
+        System.out.println("is eligible to vote.");
+
+        System.out.println();
+
+        System.out.println("End of Lesson 5.");
+    }
+}

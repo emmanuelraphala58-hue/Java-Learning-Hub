428d260973442bb61ebfda24eb90a3142048c9b7
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:39:28 +0200

    Create Lesson8.java

---
+++ src/Lesson8.java
@@ -0,0 +1,31 @@
+public class Lesson8 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 8: Arrays");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("Arrays store multiple values");
+
+        System.out.println();
+        System.out.println("Example:");
+        System.out.println("int[] numbers = {10,20,30};");
+
+        System.out.println();
+        System.out.println("Access:");
+        System.out.println("numbers[0]");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Create an array of five names.");
+
+        System.out.println();
+        System.out.println("Challenge:");
+        System.out.println("Print all array elements using a loop.");
+
+        System.out.println();
+        System.out.println("End of Lesson 8.");
+    }
+}

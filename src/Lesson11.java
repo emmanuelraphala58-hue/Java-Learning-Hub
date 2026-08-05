a79a203fda3762a7b6a119ec6ca7edbce0f57972
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:41:54 +0200

    Create Lesson11.java

---
+++ src/Lesson11.java
@@ -0,0 +1,40 @@
+public class Lesson11 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 11: Object-Oriented Programming");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("OOP has four main principles:");
+
+        System.out.println();
+        System.out.println("1. Encapsulation");
+        System.out.println("2. Inheritance");
+        System.out.println("3. Polymorphism");
+        System.out.println("4. Abstraction");
+
+        System.out.println();
+        System.out.println("These principles help");
+        System.out.println("build clean and reusable code.");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Create a class that");
+        System.out.println("uses encapsulation.");
+
+        System.out.println();
+        System.out.println("Challenge:");
+        System.out.println("Create a subclass");
+        System.out.println("that extends another class.");
+
+        System.out.println();
+        System.out.println("Congratulations!");
+        System.out.println("You have completed");
+        System.out.println("the Java Basics Course.");
+
+        System.out.println();
+        System.out.println("End of Lesson 11.");
+    }
+}

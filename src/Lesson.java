e0022329437968445ed76990bd8e7719e76e2f12
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 13:28:10 +0200

    Update Lesson.java

--- src/Lesson.java
+++ src/Lesson.java
@@ -1,4 +1,5 @@
 public class Lesson {
+
     private String title;
 
     public Lesson(String title) {
@@ -6,6 +7,23 @@ public class Lesson {
     }
 
     public void startLesson() {
-        System.out.println("Starting lesson: " + title);
+
+        System.out.println("=================================");
+        System.out.println(title);
+        System.out.println("=================================");
+
+        System.out.println("Lesson 1: What is Java?");
+        System.out.println();
+        System.out.println("Java is a programming language used");
+        System.out.println("to build desktop, web, and mobile");
+        System.out.println("applications.");
+        System.out.println();
+        System.out.println("Java is:");
+        System.out.println("- Object-Oriented");
+        System.out.println("- Platform Independent");
+        System.out.println("- Secure");
+        System.out.println("- Easy to Learn");
+        System.out.println();
+        System.out.println("End of Lesson 1.");
     }
 }

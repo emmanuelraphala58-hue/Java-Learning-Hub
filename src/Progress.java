e80fa59c5c3d95dcce602daa9eb8672cf5b6981b
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 14:10:15 +0200

    Update Progress.java

--- src/Progress.java
+++ src/Progress.java
@@ -1,11 +1,26 @@
+
 public class Progress {
-    private int completedLessons = 0;
+
+    private int lessonsCompleted;
+    private int quizzesPassed;
+
+    public Progress() {
+        lessonsCompleted = 0;
+        quizzesPassed = 0;
+    }
 
     public void completeLesson() {
-        completedLessons++;
+        lessonsCompleted++;
+    }
+
+    public void passQuiz() {
+        quizzesPassed++;
     }
 
     public void showProgress() {
-        System.out.println("Lessons completed: " + completedLessons);
+        System.out.println("\n===== Progress =====");
+        System.out.println("Lessons Completed: " + lessonsCompleted);
+        System.out.println("Quizzes Passed: " + quizzesPassed);
+        System.out.println("====================");
     }
 }

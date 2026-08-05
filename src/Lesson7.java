11264ba66570c57121c69c51eb66a24d0d0e4575
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:38:34 +0200

    Create Lesson7.java

---
+++ src/Lesson7.java
@@ -0,0 +1,37 @@
+public class Lesson7 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 7: Methods");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("Methods group code into reusable blocks.");
+
+        System.out.println();
+        System.out.println("Example:");
+
+        System.out.println("public void greet(){");
+        System.out.println("    System.out.println(\"Hello\");");
+        System.out.println("}");
+
+        System.out.println();
+        System.out.println("Methods can:");
+        System.out.println("- Accept parameters");
+        System.out.println("- Return values");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Create a method that prints");
+        System.out.println("\"Welcome to Java\".");
+
+        System.out.println();
+        System.out.println("Challenge:");
+        System.out.println("Create a method that adds");
+        System.out.println("two numbers.");
+
+        System.out.println();
+        System.out.println("End of Lesson 7.");
+    }
+}

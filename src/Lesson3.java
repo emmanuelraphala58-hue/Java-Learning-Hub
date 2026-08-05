d971048856f5e4e4633e5a9f4b80b9c1bf3735db
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:15:31 +0200

    Create Lesson3.java

---
+++ src/Lesson3.java
@@ -0,0 +1,35 @@
+public class Lesson3 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 3: Operators");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("Operators perform operations on values.");
+        System.out.println();
+
+        System.out.println("Arithmetic Operators:");
+        System.out.println("+  Addition");
+        System.out.println("-  Subtraction");
+        System.out.println("*  Multiplication");
+        System.out.println("/  Division");
+        System.out.println("%  Modulus (Remainder)");
+
+        System.out.println();
+
+        System.out.println("Example:");
+        System.out.println("int a = 10;");
+        System.out.println("int b = 5;");
+        System.out.println("System.out.println(a + b); //15");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Write a program that calculates");
+        System.out.println("the area of a rectangle.");
+
+        System.out.println();
+        System.out.println("End of Lesson 3.");
+    }
+}

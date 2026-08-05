66af8bb29bb87e74dd4affbe6773f4d24d2ec967
emmanuelraphala58-hue <emmanuelraphala58@gmail.com>
2026-08-05 22:41:08 +0200

    Create Lesson10.java

---
+++ src/Lesson10.java
@@ -0,0 +1,34 @@
+public class Lesson10 {
+
+    public void startLesson() {
+
+        System.out.println("=========================");
+        System.out.println("Lesson 10: Classes & Objects");
+        System.out.println("=========================");
+
+        System.out.println();
+        System.out.println("A class is a blueprint.");
+        System.out.println("An object is created from a class.");
+
+        System.out.println();
+        System.out.println("Example:");
+
+        System.out.println("Car car = new Car();");
+
+        System.out.println();
+        System.out.println("Objects have:");
+        System.out.println("- Attributes");
+        System.out.println("- Methods");
+
+        System.out.println();
+        System.out.println("Practice:");
+        System.out.println("Create a Student class.");
+
+        System.out.println();
+        System.out.println("Challenge:");
+        System.out.println("Create two Student objects.");
+
+        System.out.println();
+        System.out.println("End of Lesson 10.");
+    }
+}
